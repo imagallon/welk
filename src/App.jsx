a43f27098c05724ab710340c5f4d29c6f47d4ca4
imagallon <browncans@gmail.com>
2024-08-18 16:43:50 -0700

    added skeleton components for Hero, About, Sevices. Navbar is basically completed with framer motion and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import './App.css'
+import { About } from './components/About'
+import { Hero } from './components/Hero'
 import Navbar from './components/Navbar'
 
 function App(){ 
@@ -8,6 +10,9 @@ function App(){
   return (
   <div className="font-primary overflow-x-hidden">
     <Navbar/>
+    <Hero/>
+    <Services/>
+    <About/>
   </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,62 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
         const [isOpen, setIsOpen] = useState(false);
         const [activeSection, setActiveSection] = useState('home');
+        const handleToggle = () => {
+            setIsOpen(!isOpen)
+        }
+        
+        const handleCloseMenu = () => {
+            setIsOpen(false)
+        }
+
+        const handleScroll = () => {
+            const sections = ['home', 'services', 'about', 'pricing', 'testimonial'];
+            const scrollPosition = window.scrollY + 100;
+
+            sections.forEach(section => {
+                const element = document.getElementById(section);
+                if(element){
+                    const offsetTop = element.offsetTop;
+                    const height = element.offsetHeight;
+                    if(scrollPosition >= offsetTop && scrollPosition < offsetTop + height) {
+                        setActiveSection(section)
+                    }
+                }
+            })
+        }
+
+        useEffect(() => {
+            window.addEventListener('scroll', handleScroll);
+            return () => window.removeEventListener('scroll', handleScroll);
+        }, []);
+
+        const handleScrollTo = (targetId) => {
+            const targetElement = document.getElementById(targetId);
+            if(targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                })
+            }
+        }
+
         const navLinks = (
-            <ul className='font-medium flex flex-col md:flex-row lg:space-x-8 sm:space-x-4 space-y-2 md:space-y-0 p-4'>
+            <ul className='font-medium flex flex-col md:flex-row lg:space-x-8 sm:space-x-4 space-y-2 md:space-y-0 p-4 md:p-0'>
                 <li>
-                    <a href="#home"
-                    className={`text-white ${activeSection ==='home' ? 'isActive' : ''}`}>Home</a>
+                    <motion.a
+                    whileHover={{scale: 1.1}}
+                    whileTap={{scale: 0.9}}
+                    href="#home"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleCloseMenu();
+                        handleScrollTo('home')
+                    }}
+                    className={`text-white ${activeSection ==='home' ? 'isActive' : ''}`}>Home</motion.a>
                 </li>
                 <li>
                     <a href="#services"
@@ -46,7 +95,43 @@ const Navbar = () => {
                 </nav>
             </div>
             {/* button */}
+            <div className='hidden md:block'>
+                <a href="#contact" className='text-white bg-primary hover:bg-primary/90 px-4 py-2 rounded'>
+                    Contact Us
+                </a>
+            </div>
+
+            {/* hamburger menu */}
+            <div className='block md:hidden'>
+                <button 
+                onClick={handleToggle}
+                className={`text-white focus:outline-none ${isOpen ? 'border border-white' : ''}`}>
+                    <HiOutlineMenuAlt3 className='size-6' />
+                </button>
+            </div>
         </div>
+
+        {/* mobile nav items */}
+        {
+            isOpen && (
+                <nav className='absoulute top-full left-0 w-full bg-heroBg z-20 md:hidden'>
+                    <ul className='flex flex-col p-4 space-y-3'>
+                        {navLinks.props.children}
+                    <li className='py-2'>
+                        <a href="#contact"
+                        className='text-white bg-primary hover:bg-primary/90 px-4 py-4 rounded'
+                        onClick={(e) => {
+                            e.preventDefault();
+                            handleCloseMenu();
+                        }}
+                        >
+                            Contact Us
+                        </a>
+                    </li>
+                    </ul>
+                </nav>
+            )
+        }
     </header>
   )
 }
